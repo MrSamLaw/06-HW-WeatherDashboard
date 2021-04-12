1a7e38ef2d00dbc8f7890116375596e94fe39340
Mr Sam Law <sam@chongland.com>
2021-04-12 13:39:54 +1000

    Footer & Fetch updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var OWMApiKey = "&appid=8161cdf2d2a69066a121e9f145638d0a";
 
 function init() {
     getSearchHistory();
-    updateSearchHistory();
+    renderSearchHistory();
 }
 
 function getSearchHistory() {
@@ -23,41 +23,11 @@ function getSearchHistory() {
         return;
     }
 
-    console.log("Pulled Search History");
-    console.log(searchHistory);
-
     document.querySelector("#search-history").innerHTML = "";
 }
 
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
-
-    var searchInputVal = document.querySelector('#city-input').value;
-
-    if (!searchInputVal) {
-        document.querySelector(".modal-title").textContent = "Invalid Input"
-        document.querySelector(".modal-body").textContent = "Please enter a city to search for."
-        $("#myModal").modal();
-        console.error('You need a search input value!');
-        return;
-    }
-
-    searchApi(searchInputVal);
-}
-
-function updateSearchHistory(citySearch) {
-    console.log("Update Search History");
-    console.log("searchHistory Length = " + searchHistory.length);
-
-    if (!searchHistory.includes(citySearch)) {
-        console.log(searchHistory)
-        if (citySearch != null) {
-            searchHistory.push(citySearch);
-            localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-        }
-    }
-
-    document.querySelector("#search-history").innerHTML = "Search History";
+function renderSearchHistory() {
+    document.querySelector("#search-history").innerHTML = "<h5>Search History</h5>";
     searchHistory.forEach(function (citySearches) {
         var searchHistoryBtn = document.createElement("button");
         searchHistoryBtn.classList.add("search-history-btn", "btn", "btn-info", "btn-block");
@@ -75,18 +45,23 @@ function updateSearchHistory(citySearch) {
     });
 }
 
-function searchCoords(cityLat, cityLon) {
-    var requestLatLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + cityLat + cityLon + excludes + "&units=metric" + OWMApiKey;
-    fetch(requestLatLonUrl)
-        .then(function (response) {
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
 
-        })
+    var searchInputVal = document.querySelector('#city-input').value;
+
+    if (!searchInputVal) {
+        document.querySelector(".modal-title").textContent = "Invalid Input"
+        document.querySelector(".modal-body").textContent = "Please enter a city to search for."
+        $("#myModal").modal();
+        return;
+    }
+
+    searchApi(searchInputVal);
 }
 
-
 function searchApi(citySearch) {
-    // fetch request gets a list of all the repos for the node.js organization
-    // var citySearch = "Sydney";
+
     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + OWMApiKey;
 
     fetch(requestUrl)
@@ -96,54 +71,78 @@ function searchApi(citySearch) {
                     console.log(data);
                     console.log(data.city.coord.lat);
                     console.log(data.city.coord.lon);
+                    searchCoords(citySearch, data.city.coord.lat, data.city.coord.lon);
                 });
             } else {
 
                 document.querySelector(".modal-title").textContent = "Invalid Input"
-                document.querySelector(".modal-body").textContent = "Please enter a valid city name."
+                document.querySelector(".modal-body").innerHTML = "<strong>" + citySearch + "</strong>" + " is not a valid city name." + "<br />"
+                document.querySelector(".modal-body").innerHTML += "Please enter a valid city name.";
                 $("#myModal").modal();
-                console.log("Not a valid city");
-                // throw response.json();
             }
         })
-        .catch(function (error) {
-            console.log("Error");
-        });
+}
+function updateSearchHistory(citySearch) {
+    console.log("Update Search History");
+    console.log("searchHistory Length = " + searchHistory.length);
 
-    //     .then(function (data) {
-    // console.log(data);
-    // console.log(data.city.coord);
-    // console.log(data.city.coord.lat);
-    // console.log(data.city.coord.lon);
-    // citySearchLat = "lat=" + data.city.coord.lat;
-    // citySearchLon = "&lon=" + data.city.coord.lon;
-    // console.log(citySearchLat);
-    // console.log(citySearchLon);
+    if (!searchHistory.includes(citySearch)) {
+        console.log(searchHistory)
+        if (citySearch != null) {
+            searchHistory.push(citySearch);
+            localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        }
+    }
 
-    // var requestLatLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + citySearchLat + citySearchLon + excludes + "&units=metric" + OWMApiKey;
-    // console.log(requestLatLonUrl);
-    // fetch(requestLatLonUrl)
-    //     .then(function (response) {
-    //         if (!response.ok) {
-    //             throw response.json();
-    //         }
-    //         return response.json();
-    //     })
-    //     .then(function (data) {
-    //         console.log(data);
-
-    //         console.log("Current Conditions");
-    //         console.log("Temperature: " + data.current.temp);
-    //         console.log("Humidity: " + data.current.humidity);
-    //         console.log("Wind Speed: " + data.current.wind_speed);
-    //         console.log("UV Index: " + data.current.uvi);//(with scale - Favorable, Moderate, Severe)
-    //         console.log("Future Conditions(5 - Day Forecast");
-
-    //         printCurrent(citySearch, data);
-    //         printFiveDayForecast(data);
-    //     });
 
 }
+
+function searchCoords(citySearch, cityLat, cityLon) {
+    var requestLatLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + cityLat + cityLon + excludes + "&units=metric" + OWMApiKey;
+    fetch(requestLatLonUrl)
+        .then(function (response) {
+            console.log(response.json());
+            console.log("Search Coords");
+
+            // .then(function (data) {
+            //         console.log(data);
+            //         console.log(data.city.coord);
+            //         console.log(data.city.coord.lat);
+            //         console.log(data.city.coord.lon);
+            citySearchLat = "lat=" + cityLat;
+            citySearchLon = "&lon=" + cityLon;
+            //         console.log(citySearchLat);
+            //         console.log(citySearchLon);
+
+            var requestLatLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + citySearchLat + citySearchLon + excludes + "&units=metric" + OWMApiKey;
+            console.log(requestLatLonUrl);
+            fetch(requestLatLonUrl)
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw response.json();
+                    }
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+
+                    console.log("Current Conditions");
+                    console.log("Temperature: " + data.current.temp);
+                    console.log("Humidity: " + data.current.humidity);
+                    console.log("Wind Speed: " + data.current.wind_speed);
+                    console.log("UV Index: " + data.current.uvi);//(with scale - Favorable, Moderate, Severe)
+                    console.log("Future Conditions(5 - Day Forecast");
+
+                    printCurrent(citySearch, data);
+                    printFiveDayForecast(data);
+                });
+        })
+
+}
+
+
+
+
 
 function printCurrent(citySearch, city) {
 
